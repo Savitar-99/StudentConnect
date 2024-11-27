43d11549e5bc6c374c08bfdb1a1046a578ba6e9d
Joel Galán Pérez <joelgalanperez@gmail.com>
2024-11-27 13:32:44 +0100

    Visuals

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+// src/components/Footer.tsx
+import React from 'react';
+import './Footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 StudentConnect. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;

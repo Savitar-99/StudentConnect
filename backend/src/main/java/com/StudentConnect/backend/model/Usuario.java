fc5c10aaa0c0d0b513e873f6f4837af7cfef51a9
Lau Sierra <laura.sierraJ03@gmail.com>
2024-11-29 23:31:09 +0100

    ++

--- backend/src/main/java/com/StudentConnect/backend/model/Usuario.java
+++ backend/src/main/java/com/StudentConnect/backend/model/Usuario.java
@@ -6,15 +6,13 @@ public class Usuario {
     private String email;
     private String password;
 
-    // constructor vacío
+    // constructores
     public Usuario() {
         id = -1;
         nombre = "default";
         email = "default";
         password = "default";
     }
-
-    // constructor 
     public Usuario(int id, String nombre, String email, String password) {
         this.id = id;
         this.nombre = nombre;
@@ -26,31 +24,24 @@ public class Usuario {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }

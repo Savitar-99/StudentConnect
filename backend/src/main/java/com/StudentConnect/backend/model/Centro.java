1e370ce427a281690343a733b8b28b0a1a8856d7
Lau Sierra <laura.sierraJ03@gmail.com>
2024-11-29 18:33:40 +0100

    spring boot finally in working

---
+++ backend/src/main/java/com/StudentConnect/backend/model/Centro.java
@@ -0,0 +1,10 @@
+package com.StudentConnect.backend.model;
+
+public class Centro {
+    private int id;
+    private String nombre;
+    private String direccion;
+
+    // Getters y setters
+}
+
---
+++ backend/src/main/java/com/StudentConnect/backend/model/Usuario.java
@@ -0,0 +1,9 @@
+package com.StudentConnect.backend.model;
+
+public class Usuario {
+    private String email;
+    private String password;
+    private String rol;
+
+    //getters y setters
+}
